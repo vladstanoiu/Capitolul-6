d85a086ae18e86705eb8ba1abfaf2b4b230a7650
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2019-12-06 22:52:24 +0200

    Create Cerc.cpp

---
+++ Clilindrul/src/Cerc.cpp
@@ -0,0 +1,19 @@
+#include "Cerc.h"
+#include "Cilindru.h"
+#include <math.h>
+
+Cerc::Cerc(double raza)
+{
+_raza = raza;
+}
+
+double Cerc::Arie()
+{
+    return (3.14* pow(Get_raza(),2) ;
+
+}
+
+Cerc::~Cerc()
+{
+    //dtor
+}
