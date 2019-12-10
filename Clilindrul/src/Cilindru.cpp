c41d3fa853d288fe3edbfcf17c9ec1d09e10bacd
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2019-12-11 00:05:01 +0200

    Update Cilindru.cpp

--- Clilindrul/src/Cilindru.cpp
+++ Clilindrul/src/Cilindru.cpp
@@ -1,17 +1,17 @@
 #include "Cilindru.h"
 #include "Cerc.h"
 
-double Cilindru::Cilindru(double baza , double inaltime)
+
+Cilindru::Cilindru (double baza , double inaltime): _baza(baza)
 {
-_baza.Set_raza(baza);
 _inaltime = inaltime;
 }
 
-double Cilindru::Volum(){
+ double Cilindru::Volum(){
     return (_baza.Arie()*_inaltime);
 }
 
-double Cilindru::Arie()
+ double Cilindru::Arie()
 {
     return 2*_baza.Arie()+2*3.14*_baza.Get_raza()*_inaltime;
 }
